004eb2f85e6ab98ef21341970b6aaeee401804f7
Thomas Parratt <tparratt@c2r2p6.hive.fi>
2024-04-26 12:18:42 +0300

    removed pipe execution

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: tparratt <tparratt@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 10:18:38 by tparratt          #+#    #+#             */
-/*   Updated: 2024/04/26 10:32:31 by tparratt         ###   ########.fr       */
+/*   Updated: 2024/04/26 12:17:00 by tparratt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,34 +34,6 @@ void	print_split(char **split)
 	{
 		ft_printf("%s\n", split[i]);
 		i++;
-	}
-}
-
-void	execute_pipe(t_cmd *cmds, char **envp)
-{
-	int		fd[2];
-	pid_t	id;
-
-	pipe(fd);
-	id = fork();
-	if (id == 0)
-	{
-		close(fd[0]);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
-		execve(cmds->path1, cmds->cmd1, envp);
-		perror("execve failure");
-		exit(1);
-	}
-	else
-	{
-		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		wait(NULL);
-		execve(cmds->path2, cmds->cmd2, envp);
-		perror("execve failure");
-		exit(1);
 	}
 }
 

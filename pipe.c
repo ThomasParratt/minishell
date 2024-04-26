d2c0621752eb1be4e9188eb2953b555591384e44
Thomas Parratt <tparratt@c2r2p6.hive.fi>
2024-04-26 12:18:06 +0300

    changed pipe.c

--- pipe.c
+++ pipe.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-
-void	execute_pipe(char **cmd1, char **cmd2, char **envp)
+void	execute_pipe(t_cmd *cmds, char **envp)
 {
 	int		fd[2];
 	pid_t	id;
@@ -15,8 +10,8 @@ void	execute_pipe(char **cmd1, char **cmd2, char **envp)
 		close(fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
-		execve(cmd1[0], cmd1, envp);
-		perror("execve");
+		execve(cmds->path1, cmds->cmd1, envp);
+		perror("execve failure");
 		exit(1);
 	}
 	else
@@ -25,20 +20,58 @@ void	execute_pipe(char **cmd1, char **cmd2, char **envp)
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
 		wait(NULL);
-		execve(cmd2[0], cmd2, envp);
-		perror("execve");
+		execve(cmds->path2, cmds->cmd2, envp);
+		perror("execve failure");
 		exit(1);
 	}
 }
 
-int	main(int argc, char **argv, char **envp)
+void	parse_input(char *line_read, char **tokens, char **envp)
 {
-	if (argc < 3)
-		return (1);
-	char	*cmd1[] = {argv[1], NULL}; //this would contain arguments if there are any
-	char	*cmd2[] = {argv[2], NULL}; //this would contain arguments if there are any
+	char	**pipe_cmds;
+	t_cmd	*cmds;
 
-	execute_pipe(cmd1, cmd2, envp);
+	cmds = malloc(sizeof(t_cmd));
+	if (!contains_pipe(line_read))
+		execute_command(tokens, envp);
+	else if (contains_pipe(line_read))
+	{
+		pipe_cmds = ft_split(line_read, '|');
+		cmds->cmd1 = ft_split(pipe_cmds[0], ' ');
+		cmds->path1 = get_path(cmds->cmd1);
+		cmds->cmd2 = ft_split(pipe_cmds[1], ' ');
+		cmds->path2 = get_path(cmds->cmd2);
+		free_split(pipe_cmds);
+		execute_pipe(cmds, envp);
+		free_split(cmds->cmd1);
+		free_split(cmds->cmd2);
+		free(cmds->path1);
+		free(cmds->path2);
+		free(cmds);
+	}
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	char	*line_read;
+	char	*prompt;
+	char	**tokens;
+
+	argv = NULL;
+	if (argc == 1)
+	{
+		while (1)
+		{
+			prompt = create_prompt();
+			line_read = readline(prompt);
+			free(prompt);
+			add_history(line_read);
+			tokens = ft_split(line_read, ' ');
+			parse_input(line_read, tokens, envp);
+			free(line_read);
+			free_split(tokens);
+		}
+	}
 	return (0);
 }
 
